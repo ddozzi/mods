404ea7ef67b3ac0de1d4ddf5a9feaccda9d2b64e
ddozzi <69029714+ddozzi@users.noreply.github.com>
2021-09-08 00:29:52 -0400

    Delete Lastseen-Mod directory

--- Lastseen-Mod/src/main/java/com/ddozzi/lastseen/proxy/ClientProxy.java
+++
@@ -1,9 +0,0 @@
-package com.ddozzi.lastseen.proxy;
-
-public class ClientProxy extends CommonProxy {
-	@Override
-	public void registerRenders() {
-		//pass
-	}
-
-}
--- Lastseen-Mod/src/main/java/com/ddozzi/lastseen/proxy/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package com.ddozzi.lastseen.proxy;
-
-public class CommonProxy {
-	
-	public void registerRenders() {
-		
-	}
-
-}
